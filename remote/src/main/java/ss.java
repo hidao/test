1254fbfac0a753cde78874cfc242f65acfa3b4f9
win <dd>
2015-07-02 17:04:50 +0800

    cvf

---
+++ remote/src/main/java/ss.java
@@ -0,0 +1,11 @@
+/**
+ * Created by win on 2015/7/2.
+ */
+public class ss {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("-----------677--65656ytjt---");
+    }
+}
